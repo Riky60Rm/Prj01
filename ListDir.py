18241086196c1374067cfb0186cef2ff8c7c54c2
riky60 <rcigola@gmx.net>
2023-02-28 17:27:51 +0100

    Versione del 2023.02.28 - versione iniziale

--- ListDir.py
+++ ListDir.py
@@ -19,21 +19,37 @@ ShName = os.path.basename(__file__)
 # print ("Program name " + ShName)
 
 files = []  # type: List[str]
-# inputDir = '/media/FreeNas'
-inputDir = '/media/1TbF/tmp/Magazine'
-# inputDir = '/media/Qnap/Disinformazione/YouTube'
+# WorkDir = '/media/Rpi3B+Download/Download'
+# WorkDir = '/media/riky60/12Tb/tmp/pippo'
+WorkDir = '/media/ramdsk/pippo'
+# WorkDir = " "
+inputDir = ""
+inputDirCreated : bool = False
 
 # -----------------------------------------------------------------------------------------
 # define the key of sort
 # -----------------------------------------------------------------------------------------
-def sortkey(elem):
-    return elem.split('/')[7]
-
+def createDir(inputDir):
+    global inputDirCreated , WorkDir
+    cmd = "mkdir " + '"' + WorkDir + '/' + inputDir + '"'
+    rslt = os.system(cmd)
+    # rslt = 0
+    if rslt == 0 :
+        print("The directory " + WorkDir + '/' + inputDir + "has been created")
+        print(" ")
+    elif rslt == 256 :
+        print("Error the directory " + WorkDir + '/' + inputDir + "already exists")
+        exit(12)
+    else :
+        print("Error creating the directory " + WorkDir + '/' + inputDir + "return code is ", rslt)
+        exit(12)
+    inputDirCreated = True
 
 # -----------------------------------------------------------------------------------------
 # define the value of syntax string
 # -----------------------------------------------------------------------------------------
 def syntax():
+    global inputDir
     parser = ArgumentParser(prog="ListDirpy", description = """
     ------------------------- it will work on '/media/Rpi3B+Download/Download' directory ----------------------- "
     Program to move the downloaded file which are one per directory to a unique directory. the new directory 
@@ -44,59 +60,39 @@ def syntax():
        the shell will find for all the dir name which contain 'Strange' and move the files contained to the 'Strange' dir""",
                     epilog = 'Text at the bottom of help')
 
-    parser.add_argument("inputDir", help="directory that will be created",type=str)
-    parser.add_argument("-h", "--help", help="" )
+    parser.add_argument("inputDir", help="directory that will be created and where to move the files",type=str)
+    #parser.add_argument("-h", "--help", help="help" )
     args: Namespace = parser.parse_args()
+    print("inputDir = ",args.inputDir)
+    inputDir = args.inputDir
 
     return None
 
 
+syntax()
+createDir(inputDir)
 # r=root, d=directories, f = files
-for r, d, f in os.walk(inputDir):
-    print ("r value is " , r)
-    print("d value is ", d)
-    print ("f value is " , f)
-    # for file in f:
-    #     st = os.stat(os.path.join(r, file))
-    #     # print(st[ST_SIZE], file)
-    #     # print("{:14,.0f}".format(st[ST_SIZE]), file)
-    #     ModTime = time.strftime("%d/%m/%Y %I:%M:%S %p", time.localtime(st[ST_MTIME]))
-    #     files.append("{:14,.0f}".format(st[ST_SIZE]) + "  " + ModTime + "  " + os.path.join(r, file))
+for r, d, f in os.walk(WorkDir):
+    #print("r =", r, " ", type(r))
+    #print("d =", d, " ", type(d))
+    #print("f =", f, " ", type(f))
+    if r.find(inputDir) == -1 :
+        continue
+    elif len(f) == 0 :
+        continue
+    else :
+        cmd = "mv " + '"' + r + '/' + f[0] + '" ' + WorkDir + '/' + inputDir
+        rslt = os.system(cmd)
+        if rslt == 0:
+            print("file ", f[0] , " moved")
+        else :
+            print(cmd)
+            print("ERROR moving file " , f)
+        print(" ")
+        # print(cmd)
+        print("-----------------------------------------------------------------------------")
 
-sys.exit(2)
+# cp -rv /media/riky60/12Tb/tmp/pippo /media/ramdsk
 
 
-    # print("------------------ print 'files' content ------------------------")
-    # sort the list of file by name
-    # lamba split a file name in a list at the "/" delimiter and
-    #           then takes the last element of the list
-    print("=================== print 'files' content unsorted ========================")
-    print (files)
-
-    files.sort(key=lambda x: x.split('/')[-1])
-    print("------------------ print 'files' content ------------------------")
-    for f in files:
-        print(f)
-
-# -----------------------------------------------------------------------------------------
-# create the name of file
-# -----------------------------------------------------------------------------------------
-# Look at another Example
-d = datetime.datetime.today()
-# print(d.strftime("%d.%m.%Y.%H.%M.%S"))
-fName = "/media/1TbF/tmp/ElencoFile." + d.strftime("%Y.%m.%d") + ".txt"
-
-# -----------------------------------------------------------------------------------------
-# write the file
-# -----------------------------------------------------------------------------------------
-file = open(fName, "w")
-for f in files:
-    file.write(f + "\n")
-file.close()
-
-print("    ")
-print("-----------------------------------------------------------------------------------")
-print(fName, " contains the list of file in ", inputDir)
-print("-----------------------------------------------------------------------------------")
-
-sys.exit(22)
+sys.exit(0)
